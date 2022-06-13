197498bdaee0af2cd665b10562dbcd2955fb2cd3
Atusiimire <mutatinamoureen@gmail.com>
2022-06-13 08:43:14 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,15 @@
 
 void puts2(char *str)
 {
-int i = 0;
-while (*(str + i))
+int i;
+int j = 0;
+while (str[j] != '\0')
 {
-_putchar(*(str + i));
-i = i + 1;
+j++;
+}
+for (i = 0; i < j; i += 2)
+{
+_putchar(str[i]);
 }
 _putchar('\n');
 }
