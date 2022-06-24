d5f578ccf0066589cba8a67e9a3982eaddcd3677
Atusiimire <mutatinamoureen@gmail.com>
2022-06-24 02:40:14 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints its name.
+ * @argc : number of arguments.
+ * @argv : array that contains the arguments.
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+(void)argc;
+return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+int _putchar(char c);
+int is_integer(const char *str);
+int get_coin_number(int amount, int nb_coins);
+#endif
