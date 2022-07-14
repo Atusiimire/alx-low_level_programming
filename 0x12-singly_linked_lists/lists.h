3a253dcf34d7662241716b97c29ce13b427cfb56
Atusiimire <mutatinamoureen@gmail.com>
2022-07-14 11:03:02 -0700

    first commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
-#include <stddef.h>
+#ifndef _LINKED_LIST
+#define _LINKED_LIST
 
 /**
  * struct list_s - singly linked list
@@ -16,7 +15,7 @@ char *str;
 unsigned int len;
 struct list_s *next;
 } list_t;
-int _putchar(char);
+int _strlen(const char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
