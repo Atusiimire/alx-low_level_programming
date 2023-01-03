5bed2dedc8bf3f71603f646f80015449909df964
Atusiimire <mutatinamoureen@gmail.com>
2023-01-02 23:31:58 -0800

    first commit

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_arr(int *array, int left, int right);
