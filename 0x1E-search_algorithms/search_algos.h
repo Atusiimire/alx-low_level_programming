d4a41084139f98efc0a367f69563ddea43301a8e
Atusiimire <mutatinamoureen@gmail.com>
2023-01-04 01:25:31 -0800

    first commit

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -40,3 +40,4 @@ void print_arr(int *array, int left, int right);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+#endif
