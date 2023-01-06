ab4d8f8591ab6febc49dba3f04d8e54823da71a7
Atusiimire <mutatinamoureen@gmail.com>
2023-01-06 06:59:57 -0800

    first commit

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,53 +1,87 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
 
 
+skiplist_t *recurse_normal(skiplist_t *probe, skiplist_t *stop, int value);
+skiplist_t *recurse_express(skiplist_t *probe, int value);
+skiplist_t *find_list_end(skiplist_t *probe);
+
 /**
- * linear_skip - Implementing a linear search using a skip
- * list accross an express lane
- * @list: The linked list to be searched
- * @value: The value to be searched
- * Return: NULL if not found or the pointer to the first occurence
+ * linear_skip - perform search with skip list
+ * @list: list to search
+ * @value: search value
+ * Return: matching node; NULL if not found
  */
 
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-skiplist_t *fwd_ptr, *bwd_ptr, *probe_ptr, *exp_ptr;
-int end = 0;
-fwd_ptr = list;
-while (fwd_ptr != NULL)
-{
-exp_ptr = fwd_ptr->express;
-bwd_ptr = fwd_ptr;
-if (exp_ptr)
-{
-fwd_ptr = exp_ptr;
-printf("Value checked at index [%lu] = [%d]\n",
-fwd_ptr->index, fwd_ptr->n);
-}
+skiplist_t *zone = NULL;
+if (list == NULL)
+return (NULL);
+zone = recurse_express(list, value);
+if (zone->n == value)
+return (zone);
 else
-{
-end = 1;
-while (fwd_ptr->next)
-fwd_ptr = fwd_ptr->next;
+return (recurse_normal(zone, zone->express, value));
 }
-if (value < fwd_ptr->n || end)
+/**
+ * recurse_express - search express list
+ * @probe: search pointer
+ * @value: search value
+ * Return: pointer to match or match range; NULL if not in range
+ */
+skiplist_t *recurse_express(skiplist_t *probe, int value)
+{
+skiplist_t *last = NULL;
+if (probe->express == NULL)
+{
+last = find_list_end(probe);
+printf("Value found between indexes [%lu] and [%lu]\n",
+probe->index, last->index);
+return (probe);
+}
+printf("Value checked at index [%lu] = [%d]\n",
+probe->express->index, probe->express->n);
+if (probe->express->n >= value)
 {
 printf("Value found between indexes [%lu] and [%lu]\n",
-bwd_ptr->index, fwd_ptr->index);
-probe_ptr = bwd_ptr;
-while (probe_ptr->index <= fwd_ptr->index)
+probe->index, probe->express->index);
+return (probe);
+}
+else
+return (recurse_express(probe->express, value));
+}
+/**
+ * recurse_normal - search normal list
+ * @probe: search pointer
+ * @stop: endpoint of subsearch; either express node or NULL
+ * @value: search value
+ * Return: pointer to match; NULL if not found
+ */
+skiplist_t *recurse_normal(skiplist_t *probe, skiplist_t *stop, int value)
 {
+if (probe == stop)
+{
+if (stop != NULL && stop->n == value)
+return (stop);
+else
+return (NULL);
+}
 printf("Value checked at index [%lu] = [%d]\n",
-probe_ptr->index, probe_ptr->n);
-if (probe_ptr->n == value)
-return (probe_ptr);
-probe_ptr = probe_ptr->next;
-if (probe_ptr == NULL)
-break;
+probe->index, probe->n);
+if (probe->n == value)
+return (probe);
+else
+return (recurse_normal(probe->next, stop, value));
 }
-return (NULL);
-}
-return (NULL);
+/**
+ * find_list_end - find last node
+ * @probe: search pointer
+ * Return: pointer to final node
+ */
+skiplist_t *find_list_end(skiplist_t *probe)
+{
+if (probe->next == NULL)
+return (probe);
+else
+return (find_list_end(probe->next));
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include <math.h>
 
+/* helpers */
+void print_array(int *array, size_t inc, size_t size);
+
 /**
  * struct listint_s - singly linked list
  * @n: Integer
@@ -36,9 +39,8 @@ struct skiplist_s *next;
 struct skiplist_s *express;
 } skiplist_t;
 
-skiplist_t *linear_skip(skiplist_t *list, int value);
 
-listint_t *jump_list(listint_t *list, size_t size, int value);
+/* main functions */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_arr(int *array, int left, int right);
@@ -46,4 +48,7 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif
